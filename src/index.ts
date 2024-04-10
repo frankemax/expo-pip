3be8e033faacd6752dc53e14a89d4d6c9820daae
Max <max.franke@acad.pucrs.br>
2024-04-10 17:25:36 -0300

    feat: cleanup more boilerplate code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import ExpoPipModule from "./ExpoPipModule";
 
-// Import the native module. On web, it will be resolved to ExpoPip.web.ts
-// and on native platforms to ExpoPip.ts
-import ExpoPipModule from './ExpoPipModule';
-import ExpoPipView from './ExpoPipView';
-import { ChangeEventPayload, ExpoPipViewProps } from './ExpoPip.types';
-
-// Get the native constant value.
-export const PI = ExpoPipModule.PI;
-
-export function hello(): string {
-  return ExpoPipModule.hello();
+export function getTheme(): string {
+  return ExpoPipModule.getTheme();
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoPipModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoPipModule ?? NativeModulesProxy.ExpoPip);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoPipView, ExpoPipViewProps, ChangeEventPayload };
